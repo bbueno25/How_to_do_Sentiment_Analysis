400efc4886f74b29b7b2ef4998ff1514703415d1
B. Bueno <bbueno5000@gmail.com>
2020-01-14 23:36:36 -0700

    deleted unpickle.py

--- src/unpickle.py
+++
@@ -1,7 +0,0 @@
-from tflearn.datasets import imdb
-import pandas
-
-# IMDB Dataset loading
-train, test, _ = imdb.load_data(path='imdb.pkl', n_words=10000, valid_portion=0.1)
-
-print (test[0][0:10])
